afe1df04389abc110eef0342514d48af9b41dabe
NeoSphere0 <dan.kirkpatrick96@gmail.com>
2017-03-03 09:38:36 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=unifiedhostsadblock
+MODID=AndroidAdblocker
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -50,8 +50,8 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Unified Hosts Adblock     "
-  ui_print "          (Zackptg5)           "
+  ui_print "       Android Adblocker       "
+  ui_print "          (NeoSphere)          "
   ui_print "*******************************"
 }
 
